c9c0a9da8898458577687d757df5922495dc06ad
Rodolfo-Rodrigues <rodofix1@gmail.com>
2025-07-22 10:01:05 -0300

    concanetando String + inteiros
    Programa exibe uma messagem que apresenta a chamada de uma string e um valor inteiro.

---
+++ int2.py
@@ -0,0 +1,5 @@
+#programa demonstra a concatenação de Strings mais inteiros
+
+idade= 34
+message= " feliz "  + str(idade)  +  " anos! "  # aspas duplas devem apresentar espaço para evitar erros
+print(message)
