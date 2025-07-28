a9968a886f4d238cc6e682554a16388c9b7a0b04
Rodolfo-Rodrigues <rodofix1@gmail.com>
2025-07-28 10:54:21 -0300

    adicionando elementos a uma lista ja criada.
    Adicina novos  elementos a uma lista ja  criada .

---
+++ list4.py
@@ -0,0 +1,5 @@
+# modificando lista já criada
+
+bicicletas= ["caloi", "solar", "zuma"]
+bicicletas[0]= "florida" # faz a troca  de valores adicionando florida a o valor inicial da lista
+print(bicicletas)
